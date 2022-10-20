0f4bfe0c7fcdcde6237f14a3976cfa2362e44a7b
himaxisakariya <114063966+himaxisakariya@users.noreply.github.com>
2022-10-20 12:59:17 +0530

    Commit Initaial

--- lib/constant/widget/snackbar.dart
+++ lib/constant/widget/snackbar.dart
@@ -5,6 +5,7 @@ snackbar(BuildContext context, {String? text}) async {
     content: Text(
       '$text',
       style: const TextStyle(color: Colors.black),
+
     ),
     backgroundColor: const Color(0xffE5E4E2),
     behavior: SnackBarBehavior.floating,
